8170a2dac1c676a474ebf1365a725c32d6c9947f
yepremio <yeprem86@gmail.com>
2024-06-30 17:18:45 -0700

    initial commit

---
+++ src/main.py
@@ -0,0 +1,8 @@
+print("hello world")
+from textnode import textnode
+
+main()
+
+print(TextNode(This is a text node, bold, https://www.boot.dev))
+
+
---
+++ src/textnode.py
@@ -0,0 +1,11 @@
+class TextNode:
+    def __init__(text, text_type, url)
+    self.text = text
+    self.text_type = text_type
+    self.url = url
+
+    def __eq__()
+        pass
+        
+    def __repr__()
+        return TextNode(TEXT, TEXT_TYPE, URL)
