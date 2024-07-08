9a2a31861506675d807312ece7e2d890456048a1
yepremio <yeprem86@gmail.com>
2024-07-07 21:29:31 -0700

    Add Changes

--- src/textnode.py
+++ src/textnode.py
@@ -21,3 +21,7 @@ class TextNode:
         )
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == text_type_text:
+        return LeafNode(None, text_node.text)
