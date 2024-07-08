5c9253046ef85a76721f7458ad44f024a2ec43c8
yepremio <yeprem86@gmail.com>
2024-07-07 21:04:03 -0700

    Add parentnode + test

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -49,15 +49,14 @@ class ParentNode(HTMLNode):
         if self.children is None:
             raise ValueError("Error: missing children")
 
-        if self.children == []:
-            return
-        return f"{ParentNode.to_html(self.children)}"
+        children_html = ""
 
-
-
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
 
     def __repr__(self):
-        return f"ParentNode({self.tag}, {self.children}, {self.props})"
+        return f"ParentNode({self.tag}, childre:{self.children}, {self.props})"
 
 
 
