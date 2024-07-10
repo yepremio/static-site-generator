6f948c249f95e20301a94a1f8bd2bcb27a609ae2
yepremio <yeprem86@gmail.com>
2024-07-09 20:01:00 -0700

    Added test for text to html node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -8,6 +8,7 @@ from textnode import (
     text_type_code,
     text_type_image,
     text_type_link,
+    text_node_to_html_node,
 )
 
 
@@ -38,6 +39,28 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual("TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
 
-    
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("this is a text node", text_type_text)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+        
+    def test_image(self):
+        node = TextNode("This is an image", text_type_image, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
+        )
+    def test_bold(self):
+        node = TextNode("This is bold", text_type_bold)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
+        
+
 if __name__ == "__main__":
     unittest.main()
