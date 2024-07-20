d532a80cc4e424c26c824f60a15652f812aea206
yepremio <yeprem86@gmail.com>
2024-07-20 09:50:00 -0700

    Added markdown to html node

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,11 +1,12 @@
-from textnode import (
-    text_type_text,
-    text_type_bold,
-    text_type_italic,
-    text_type_code,
-    text_type_image,
-    text_type_link,
-)
+from htmlnode import HTMLNode
+
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_olist = "ordered_list"
+block_type_ulist = "unordered_list"
 
 def markdown_to_blocks(markdown):
     block_list = []
@@ -26,17 +27,70 @@ def block_to_block_type(markdown):
                 break
         if count_hashes <= 6 and markdown[count_hashes] == " ":
             return "heading"
-
     elif markdown.startswith("```") and markdown.endswith("```"):
-        return text_type_code 
-        
+        return block_type_code
     elif all(line.startswith(">") for line in markdown.split("\n")):
         return "quote"
-        
     elif all(line.startswith("* ") or line.startswith("- ") for line in markdown.split("\n")):
-        return "unordered list"
-        
+        return block_type_ulist 
     elif all(line.split(". ")[0].isdigit() and line.startswith(str(index) + ". ") for index, line in enumerate(markdown.split("\n"), start=1)):
-        return "ordered_list"
+        return block_type_olist
     else:
         return "paragraph"
+
+def text_to_children(text):
+    if text 
+
+
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    nodes = []
+
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        
+        if block_type == block_type_paragraph:
+            node = HTMLNode("p")
+            node.children = text_to_children(block)
+        
+        elif block_type == block_type_heading:
+            node = HTMLNode("h")
+            node.children = text_to_children(block)
+
+        elif block_type == block_type_code:
+            node = HTMLNode("code")
+            node.children = text_to_children(block)
+
+        elif block_type == block_type_quote:
+            node = HTMLNode("blockquote")
+            node.children = text_to_children(block)
+
+        elif block_type == block_type_olist:
+            node = HTMLNode("ol")
+            node.children = text_to_children(block)
+
+        elif block_type == block_type_ulist:
+            node = HTMLNode("ul")
+            node.children = text_to_children(block)
+
+        else:
+
+        nodes.append(node)
+
+        parent_node = HTMLNode("div")
+        parent_node.children = nodes
+
+        return parent_node
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,7 +1,15 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks
-
+from markdown_blocks import (
+    markdown_to_blocks,
+    block_to_block_type,
+    block_type_paragraph,
+    block_type_heading,
+    block_type_code,
+    block_type_olist,
+    block_type_ulist,
+    block_type_quote,
+)
 class TestMarkdownToHTML(unittest.TestCase):
     def test_markdown_to_blocks(self):
         nodes = markdown_to_blocks(
@@ -13,6 +21,20 @@ class TestMarkdownToHTML(unittest.TestCase):
             "* This is the first list item in a list block\n* This is a list item\n* This is another list item"
         ]
         self.assertEqual(nodes, expected_nodes)
-
+        
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), block_type_heading)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), block_type_code)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), block_type_quote)
+        block = "* list\n* items"
+        self.assertEqual(block_to_block_type(block), block_type_ulist)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), block_type_olist)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), block_type_paragraph)
+        
 if __name__ == "__main__":
     unittest.main()
