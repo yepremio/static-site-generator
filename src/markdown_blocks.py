72c6a78395f331278f7cecec14b0915032cad709
yepremio <yeprem86@gmail.com>
2024-07-21 08:47:34 -0700

    fixed errors

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -10,13 +10,14 @@ block_type_olist = "ordered_list"
 block_type_ulist = "unordered_list"
 
 def markdown_to_blocks(markdown):
-    block_list = []
-    split_str = markdown.split('\n\n')
-    for str in split_str:
-        if str != '\n\n':
-            block_list.append(str)
-    clean_list = [s for s in block_list if s.strip()]
-    return clean_list
+    blocks = markdown.split("\n\n")
+    filtered_blocks = []
+    for block in blocks:
+        if block == "":
+            continue
+        block = block.strip()
+        filtered_blocks.append(block)
+    return filtered_blocks
 
 def block_to_block_type(markdown):
     if markdown.startswith("#"):
@@ -27,17 +28,17 @@ def block_to_block_type(markdown):
             else:
                 break
         if count_hashes <= 6 and markdown[count_hashes] == " ":
-            return "heading"
+            return block_type_heading
     elif markdown.startswith("```") and markdown.endswith("```"):
         return block_type_code
     elif all(line.startswith(">") for line in markdown.split("\n")):
-        return "quote"
+        return block_type_quote
     elif all(line.startswith("* ") or line.startswith("- ") for line in markdown.split("\n")):
         return block_type_ulist 
     elif all(line.split(". ")[0].isdigit() and line.startswith(str(index) + ". ") for index, line in enumerate(markdown.split("\n"), start=1)):
         return block_type_olist
     else:
-        return "paragraph"
+        return block_type_paragraph
 
 
 def markdown_to_html_node(markdown):
