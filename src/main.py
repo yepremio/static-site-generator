23c2812e219b5f8794991521e69aceecbacfd467
yepremio <yeprem86@gmail.com>
2024-07-23 09:37:32 -0700

    changes

--- src/main.py
+++ src/main.py
@@ -1,6 +1,21 @@
-from textnode import textnode, text_type_bold
+from textnode import TextNode
 
 def main():
+    
+    def function():
+    # base case:
+    # os.listdir is a list of names given by path
+    # os.path.exists returns True if path refers to an existing one
+    # os.mkdir create directory
+    # shutil.copy () 
+    # shutil.rmtree() deletes entire dir tree 
+    # os.path.join() joins path segments 
+    # os.path.isfile()return true if path is an existing regular fil 
+    #
+    copy all contents to dest dir but first
+    del all contents in destination dir
+    copy all files and subdir
+
     node = TextNode(This is a text node, bold, https://www.boot.dev)
     print(node)
 
