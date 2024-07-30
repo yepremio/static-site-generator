9ab531b04de57bbe719e85ba931dd6f77c18a6c1
yepremio <yeprem86@gmail.com>
2024-07-30 15:53:52 -0700

    changes

--- src/main.py
+++ src/main.py
@@ -1,33 +1,45 @@
-from textnode import TextNode
+import shutil
+import os
 
 def main():
-    
-    def function():
+
+    def copy_static_contents(src, dst):
+        # Check to see if destination exists.
+        
+        if os.path.exists(dst):
+        # Remove dir and contents 
+
+            shutil-rmtree(dst)
+
+        # Crete destination dir.
+
+        os.mkdir(dst)
+
+        # recursive logic 
+
+   if does not have dir or have files
+        return
+        copy contents to destination
+        call recursive function 
+
+
     # base case:
     # os.listdir is a list of names given by path
     # os.path.exists returns True if path refers to an existing one
-    # os.mkdir create directory
-    # shutil.copy () 
-    # shutil.rmtree() deletes entire dir tree 
     # os.path.join() joins path segments 
     # os.path.isfile()return true if path is an existing regular fil 
-    #
     os.path.isfile()
     adding additional changes to this line.
     os.path.exists 
     
-    shutil.rmtree()
     
     os.path.join()
 
-    shutil.copy() 
 
     copy all contents to dest dir but first
     del all contents in destination dir
     copy all files and subdir
 
-    node = TextNode(This is a text node, bold, https://www.boot.dev)
-    print(node)
 
 main()
 
