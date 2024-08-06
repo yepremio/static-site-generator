aeb504d26e155c5d83caa31c1c2dbb93b6a3f075
yepremio <yeprem86@gmail.com>
2024-08-06 09:52:09 -0700

    changes

--- src/generate_page.py
+++ src/generate_page.py
@@ -17,4 +17,10 @@ def extract_title(markdown):
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
-
+#read markdown from_path and store in variable
+#read template file at template_path and store in variable
+# markdown_to_html_node and .to_html() method to convert markdown file to an html string
+# use extract_title function to grab the title of the page.
+# replace the {{ Title }} and {{ Content }} placeholders with the html generated
+# write the new full HTML page to a file at dest_path
+#
